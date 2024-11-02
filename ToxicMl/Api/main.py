f148fb6501f319764be568300673929990a55b5b
Tibor Cus <tibor.cus@itsv.at>
2024-11-02 14:08:43 +0100

    rename files

--- ToxicMl/Api/main.py
+++ ToxicMl/Api/main.py
@@ -1,7 +1,32 @@
+import os
+import sys
+# insert root directory into python module search path
+sys.path.insert(1, os.getcwd())
+
+
+
 from fastapi import FastAPI
+from ToxicMl.MLmodels.base import Endpoint, MlModel, Prediction
+from ToxicMl.MLmodels.randomModel import RandomModel
+randomModel = RandomModel()
+
 
 app = FastAPI()
+
+
 
 @app.get("/")
 async def root():
 	return {"message": "Hello world"}
+
+
+@app.get("/mlmodels")
+async def getMlModels() -> list[MlModel]:
+	return [
+		MlModel(name="RandomModel", endpoint=[Endpoint.endpoint1, Endpoint.endpoint2])
+	]
+
+@app.post("/prediction/randommodel/")
+async def getPredictionsRandomModel(smiles: list[str]) -> list[Prediction]:
+	return randomModel.predictBatch(smiles)
+	
