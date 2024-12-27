dfe5c7f920f97cfbdf916c892d1f5bd93df43785
Cus, Tibor <cus.tibor@outlook.com>
2024-12-27 12:11:39 +0100

    update main.py

--- ToxicMl/Api/main.py
+++ ToxicMl/Api/main.py
@@ -1,32 +1,33 @@
 import os
 import sys
-# insert root directory into python module search path
-sys.path.insert(1, os.getcwd())
+
+from ToxicMl.MLmodels.Hiv import HivGNNModel
+from ToxicMl.MLmodels.Lipo import LipoGCNModel
+from pathlib import Path
+import streamlit as st
+import pandas as pd
 
 
+if __name__ == "__main__":
+	model = HivGNNModel(Path("saved_models/deep/HIV_ChemAttention_5-128.pt"))
+	modelReg = LipoGCNModel(Path("saved_models/deep/LIPO_GCN_5-128.pt"))
+	df = pd.DataFrame(columns=["SMILE", "Hiv Prediction", "Lipo Prediction"])
+	st.title('ToxicML')
+	uploaded_file = st.file_uploader("Choose a file")
 
-from fastapi import FastAPI
-from ToxicMl.MLmodels.base import Endpoint, MlModel, Prediction
-from ToxicMl.MLmodels.randomModel import RandomModel
-randomModel = RandomModel()
+	table = st.table(df)
+
+	if uploaded_file is not None:
+		# Can be used wherever a "file-like" object is accepted:
+		dataframe = pd.read_csv(uploaded_file)
+		res = []
+		for el in dataframe["smiles"].values.tolist():
+			res.append({
+				"SMILE" : el,
+				"Hiv Prediction" : model.predict(el),
+				"Lipo Prediction" : modelReg.predict(el)
+			})
+		new_df = pd.DataFrame(res)
+		table.add_rows(new_df)
 
 
-app = FastAPI()
-
-
-
-@app.get("/")
-async def root():
-	return {"message": "Hello world"}
-
-
-@app.get("/mlmodels")
-async def getMlModels() -> list[str]:
-	return [
-		"RandomModel"
-	]
-
-@app.post("/inference/randommodel/")
-async def getPredictionsRandomModel(smiles: list[str]) -> list[Prediction]:
-	return randomModel.predictBatch(smiles)
-	
